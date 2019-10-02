788648d3857a4d950e0f966e02cb8355f1678bf6
Harshit Gakhar <30308391+Harshit-Gakhar@users.noreply.github.com>
2019-10-02 06:33:50 +0530

    Comments
    Github didn't display tabs properly, so all the tabs have been replaced by spaces

--- INSTA_DP_VIEWER.py
+++ INSTA_DP_VIEWER.py
@@ -2,16 +2,17 @@ import requests
 import datetime
 import os
 #Author: Harshit-Gakhar
-un=input("Enter User Name: ")												#asks user to enter username
-sourceCode = str(requests.get('https://www.instagram.com/'+un+"/").text)	#gets the source code of the webpage of given user 
-date = datetime.datetime.now()												#gets date to be saved in filename
-time=str(date.day)+"-"+ str(date.month) + "-" +str(date.year) 				#time, as string, in readble form
-filename = un + " ({})".format(time) + ".jpg"								#filename is the final name of file
-left=sourceCode.find("pic_url_hd")+13										#left and right are the indices in source code, which has url of DP
+un=input("Enter User Name: ")                                               #asks user to enter username
+sourceCode = str(requests.get('https://www.instagram.com/'+un+"/").text)    #gets the source code of the webpage of given user 
+date = datetime.datetime.now()                                              #gets date to be saved in filename
+time=str(date.day)+"-"+ str(date.month) + "-" +str(date.year)               #time, as string, in readble form
+filename = un + " ({})".format(time) + ".jpg"                               #filename is the final name of file
+left=sourceCode.find("pic_url_hd")+13                                       #left and right are the indices in source code, which has url of DP
 right=sourceCode.find("requested_by_viewer")-3
-hdurl=sourceCode[left:right]												#hdurl is the url of the DP 
-response=requests.get(hdurl)												#reponse has the Display Picture Stored
+hdurl=sourceCode[left:right]                                                #hdurl is the url of the DP 
+response=requests.get(hdurl)                                                #reponse has the Display Picture Stored
 f = open(filename,'wb')
-f.write(response.content)													#writing image in a image file called filename
+f.write(response.content)                                                   #writing image in a image file called filename
 f.close()
-os.startfile(filename)														#to open the image
+os.startfile(filename)                                                      #to open the image
+
