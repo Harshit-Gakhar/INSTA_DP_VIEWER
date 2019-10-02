fcc6be84d969fa3d1f4b68fd21ed7d94de4c726d
Harshit Gakhar <30308391+Harshit-Gakhar@users.noreply.github.com>
2019-10-02 06:25:59 +0530

    Comments Cleaner

--- INSTA_DP_VIEWER.py
+++ INSTA_DP_VIEWER.py
@@ -1,29 +1,17 @@
 import requests
 import datetime
 import os
-"""
-@author: Gakhar's
-"""  
-#asks user to enter username
-un=input("Enter User Name: ")
-#gets the source code of the webpage of given user 
-sourceCode = str(requests.get('https://www.instagram.com/'+un+"/").text)
-#gets date to be saved in filename
-i = datetime.datetime.now()
-time=str(i.day)+"-"+ str(i.month) + "-" +str(i.year)+"("+str(i.hour)+"_"+str(i.minute)+")" #time, as string, in readble form
-#filename is the final name of file
-filename = un + str(time) + ".jpg"
-#left and right are the indices in source code, which has url of DP
-left=sourceCode.find("pic_url_hd")+13
+#Author: Harshit-Gakhar
+un=input("Enter User Name: ")												#asks user to enter username
+sourceCode = str(requests.get('https://www.instagram.com/'+un+"/").text)	#gets the source code of the webpage of given user 
+date = datetime.datetime.now()												#gets date to be saved in filename
+time=str(date.day)+"-"+ str(date.month) + "-" +str(date.year) 				#time, as string, in readble form
+filename = un + " ({})".format(time) + ".jpg"								#filename is the final name of file
+left=sourceCode.find("pic_url_hd")+13										#left and right are the indices in source code, which has url of DP
 right=sourceCode.find("requested_by_viewer")-3
-#hdurl is the url of the DP
-hdurl=sourceCode[left:right]
-#reponse has the Display Picture Stored    
-response=requests.get(hdurl)
-#writing image in a image file called filename
+hdurl=sourceCode[left:right]												#hdurl is the url of the DP 
+response=requests.get(hdurl)												#reponse has the Display Picture Stored
 f = open(filename,'wb')
-f.write(response.content)
+f.write(response.content)													#writing image in a image file called filename
 f.close()
-#to open the image
-os.startfile(filename)
-
+os.startfile(filename)														#to open the image
